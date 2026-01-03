64d01cfedf9bf69d838ef109bff58d258ed2e976
youssef25225 <110260642+youssef25225@users.noreply.github.com>
2026-01-04 01:23:17 +0200

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,21 +1,24 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForTokenClassification
 from transformers import pipeline
-arabic = pipeline('text-classification', model='CAMeL-Lab/bert-base-arabic-camelbert-da-sentiment')
-english = pipeline('text-classification', model='distilbert-base-uncased-finetuned-sst-2-english')
-def analyze_sentiment(text):
-    try:
-        if any('\u0600' <= char <= '\u06FF' for char in text):
-            result = arabic(text)[0]
-        else:
-            result = english(text)[0]
-        label = result['label']
-        confidence = result['score']
-        return {"label": label, "confidence": confidence}
-    except Exception as e:
-        return f"Error in sentiment analysis: {str(e)}"
+class SentimentAnalysis:
+    def __init__(self):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.tokenizer = AutoTokenizer.from_pretrained("yangheng/deberta-v3-base-absa-v1.1")
+        self.model = AutoModelForTokenClassification.from_pretrained("yangheng/deberta-v3-base-absa-v1.1").to(self.device)
+        self.nlp = pipeline("sentiment-analysis", model=self.model, tokenizer=self.tokenizer)
+
+    def main(self):
+        text = input("Enter your text: ")
+        if not text or not text.strip():
+            print("Please enter a valid text")
+            return
+        try:
+            result = self.nlp(text)
+            print(result)
+        except Exception as e:
+            print("Error in Sentiment Analysis: ", e)
+
 if __name__ == "__main__":
-    test_text_en = "I love programming! It's so much fun."
-    test_text_ar = "أنا أحب البرمجة! إنه ممتع جدا."
-    result_en = analyze_sentiment(test_text_en)
-    result_ar = analyze_sentiment(test_text_ar)
-    print(f"English Text: {test_text_en}\nSentiment Analysis: {result_en}\n")
-    print(f"Arabic Text: {test_text_ar}\nSentiment Analysis: {result_ar}\n")    
+    sentiment = SentimentAnalysis()
+    sentiment.main()
